46e975db39b027d7544ede7d001fc7d66e65c943
Piyush Sharma <sharma.076piyush@gmail.com>
2024-10-23 09:46:26 +0530

    is linked list palindrome

--- src/main/java/piyush/LinkedListProblems.java
+++ src/main/java/piyush/LinkedListProblems.java
@@ -1,5 +1,6 @@
 package piyush;
 
+	
 public class LinkedListProblems {
 
 	public static ListNode middleNode(ListNode head) {
@@ -49,6 +50,55 @@ public class LinkedListProblems {
             }
         }
         return null;
+    }
+	
+	public ListNode reverse(ListNode head) {
+        if(head == null || head.next == null)
+            return head;
+        ListNode newH = reverse(head.next);
+        ListNode front = head.next;
+        front.next = head;
+        head.next = null;
+        return newH;
+    }
+	
+	public boolean isPalindrome(ListNode head) {
+		/*
+		 * Brute force : keep all the values in a stack while traversing linkedlist.
+		 * again traverse linkedlist, if stack.pop on each iteration is not same as current node, return false.
+		 * 
+		 * Time = O(2) Space = O(n)
+		 * 
+		 * Optimal : get to the node before the middle of list, after middle reverse the list.
+		 * iterate from head, to middle and at the same time also see from middle to end of list.
+		 * compare the two, if same then reverse back second half of list to normal and return true/false.
+		 * 
+		 * Time = O(2n)
+		 * Space - O(1)
+		 * 
+		 * */
+		
+        if(head == null || head.next == null)
+            return true;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next != null && fast.next.next != null){ //O(n/2)
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode tempHead = reverse(slow.next); //O(n/2)
+        ListNode start = head;
+        ListNode middle = tempHead;
+        while(middle != null){ //O(n/2)
+            if(middle.val != start.val){
+                reverse(tempHead); //O(n/2) only once
+                return false;
+            }
+            middle = middle.next;
+            start = start.next;
+        }
+        reverse(tempHead);//O(n/2) only once
+        return true;
     }
 	
 }
