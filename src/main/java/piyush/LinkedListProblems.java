58b817af7177bbc7ecd30856e7a6eb6078278c61
Piyush Sharma <sharma.076piyush@gmail.com>
2024-10-23 08:29:36 +0530

    loop in linked list

--- src/main/java/piyush/LinkedListProblems.java
+++ src/main/java/piyush/LinkedListProblems.java
@@ -22,4 +22,33 @@ public class LinkedListProblems {
         return slow;
     }
 	
+	public ListNode detectCycle(ListNode head) {
+		/*
+		 * Find starting point of the loop in linked list.
+		 * return null if loop does not exist.
+		 * 
+		 * Brute force : keep map of to save nodes while visiting them.
+		 * 
+		 * Optimal : slow and fast pointer.
+		 * Time = O(n) space = O(n)
+		 * 
+		 * */
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next !=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow==fast){
+                slow = head;
+                while(slow != fast){
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+	
 }
