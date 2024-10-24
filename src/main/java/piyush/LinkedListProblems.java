b7d67f05b1cc1b4486e57dffadd9342ce6aab17b
Piyush Sharma <sharma.076piyush@gmail.com>
2024-10-24 07:16:32 +0530

    delete nth or middle node from linkedlist

--- src/main/java/piyush/LinkedListProblems.java
+++ src/main/java/piyush/LinkedListProblems.java
@@ -102,6 +102,14 @@ public class LinkedListProblems {
     }
 	
 	public static ListNode oddEvenList(ListNode head) {
+		
+		/*
+		 * convert 1->2->3->4->5 to 1->3->5->2->4
+		 * 
+		 * 
+		 * 
+		 * */
+		
         if(head == null || head.next == null)
             return head;
         ListNode evenHead = head.next;
@@ -115,6 +123,69 @@ public class LinkedListProblems {
         even = even.next;
        }
         odd.next = evenHead;
+        return head;
+    }
+	
+	public ListNode removeNthFromEnd(ListNode head, int n) {
+		/*
+		 * Brute force : in one pass calculate size on list.
+		 * using formula nth node from end is Size-n+1 from front,
+		 * take a pointer to one prev and remove links.
+		 * 
+		 * time = O(2n)
+		 * 
+		 * Optimal : using a window of size n, consider boundary conditions.
+		 * take previous pointer and tail pointer as window. remove links from
+		 * previous pointer.
+		 * 
+		 * Time = O(n)
+		 * Space = O(1)
+		 * 
+		 * */
+		
+        if(head.next == null)
+            return null;
+        ListNode tail = head;
+        ListNode onePrev = head;
+        for(int i=0; i<n; i++){
+            tail = tail.next;
+        }
+        if(tail == null){
+            return head.next;
+        }
+        while(tail.next != null){
+            tail = tail.next;
+            onePrev = onePrev.next;
+        }
+        onePrev.next = onePrev.next.next;
+        return head;
+    }
+	
+	public ListNode deleteMiddle(ListNode head) {
+		
+		/*
+		 * You are given the head of a linked list. Delete the middle node, and return the head of the modified linked list.
+		 * For n = 1, 2, 3, 4, and 5, the middle nodes are 0, 1, 1, 2, and 2, respectively.
+		 * 
+		 * Brute force : two pass algo, to find total count and navigate to node one before middle.
+		 * Time = O(2n)
+		 * 
+		 * Optimal : use slow and fast pointer to reach end of list, since we need to keep previous one before
+		 * the middle node, create a window of 1 in slow and fast.
+		 * 
+		 * Time = O(n)
+		 * 
+		 * */
+		
+        if(head.next == null)
+            return null;
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        slow.next = slow.next.next;
         return head;
     }
 	
