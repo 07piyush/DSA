6295f816c3674b58b3b79bc1fa7c2109063ab4d2
Piyush Sharma <sharma.076piyush@gmail.com>
2024-10-28 10:05:56 +0530

    astroid collision problem

--- src/main/java/piyush/StackUsingQueue.java
+++ src/main/java/piyush/StackUsingQueue.java
@@ -225,4 +225,29 @@ public class StackUsingQueue {
          return pse;
      }
 
+     public int[] asteroidCollision(int[] asteroids) {
+         
+    	 /*
+    	  * 735. Asteroid Collision
+    	  * 
+    	  *  
+    	  *
+    	  */
+         List<Integer> stk = new ArrayList<>();
+         for(int i = 0; i<asteroids.length; i++){
+             if(asteroids[i] > 0){
+                 stk.add(asteroids[i]);
+             }
+             else{
+                 while(!stk.isEmpty() && stk.get(stk.size()-1) > 0 && Math.abs(asteroids[i]) > stk.get(stk.size()-1)){
+                     stk.remove(stk.size()-1);
+                 }
+                 if(!stk.isEmpty() && stk.get(stk.size()-1) == Math.abs(asteroids[i]))
+                     stk.remove(stk.size()-1);
+                 else if(stk.size()==0 || stk.get(stk.size()-1) < 0)
+                     stk.add(asteroids[i]);
+             }
+         }
+         return stk.stream().mapToInt(Integer::intValue).toArray();
+     }
 }
