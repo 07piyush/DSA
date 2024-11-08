31b3de8cb6298e1020b0a9ae5f62e0e9acc682f4
Piyush Sharma <sharma.076piyush@gmail.com>
2024-11-08 08:55:51 +0530

    search in rotated sorted array

--- src/main/java/searchingAlgos/Search.java
+++ src/main/java/searchingAlgos/Search.java
@@ -38,7 +38,7 @@ public class Search {
 		 * aka lower bound is the smallest index such that nums[index] >= target.
 		 * aka first occurace of a number in a sorted array.
 		 * 
-		 * 
+		 * aka Find first position of an element in a sorted array.
 		 * 
 		 * */
 		int firstOcc = -1;
@@ -66,6 +66,8 @@ public class Search {
 		 * aka last occurrence of an element in a sorted array.
 		 * aka upper bound of a target in a sorted array.
 		 * 
+		 * aka Find last position of an element in a sorted array.
+		 * 
 		 * */
 
 		int lastOcc = -1;
@@ -87,6 +89,11 @@ public class Search {
 	}
 	
 	public int[] searchRange(int[] nums, int target) {
+		/*
+		 * Find first and last position of an element in a sorted array.
+		 * 
+		 * */
+		
 		int[] result = new int[2];
 		
 		result[0] = lowestIndexOfTargetIterative(nums, target);
@@ -94,4 +101,46 @@ public class Search {
 		
 		return result;
 	}
+	
+	public int search(int[] nums, int target) {
+		/*
+		 * Search in Rotated Sorted Array.
+		 * There is an integer array nums sorted in ascending order (with distinct values).
+		 * array is rotated by random number.
+		 * 
+		 * Given the array nums after the possible rotation and an integer target, 
+		 * return the index of target if it is in nums, or -1 if it is not in nums.
+		 * 
+		 * Example : nums = [4,5,6,7,0,1,2], target = 0 
+		 * Output: 4
+		 * 
+		 * 
+		 * */
+		
+        int left = 0;
+        int right = nums.length-1;
+
+        while(left <= right){
+            int mid = (left+right)/2;
+
+            if(nums[mid] == target){
+                return mid;
+            }
+            //if left part is sorted
+            if(nums[left] <= nums[mid]){
+                if(nums[left] <= target && target < nums[mid])
+                    right = mid-1;
+                else
+                    left = mid+1;
+            }
+            else{
+                if(nums[mid] < target && target <= nums[right])
+                    left = mid +1;
+                else
+                    right = mid-1;
+            }
+        }
+
+        return -1;
+    }
 }
