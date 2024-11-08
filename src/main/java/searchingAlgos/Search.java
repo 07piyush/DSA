003178d010f44cbb72ae4813caad3e1f4e9af400
Piyush Sharma <sharma.076piyush@gmail.com>
2024-11-08 09:31:43 +0530

    search in rotated sorted array II

--- src/main/java/searchingAlgos/Search.java
+++ src/main/java/searchingAlgos/Search.java
@@ -102,7 +102,7 @@ public class Search {
 		return result;
 	}
 	
-	public int search(int[] nums, int target) {
+	public int searchInRotatedSortedArrayWithUniqueElements(int[] nums, int target) {
 		/*
 		 * Search in Rotated Sorted Array.
 		 * There is an integer array nums sorted in ascending order (with distinct values).
@@ -114,6 +114,15 @@ public class Search {
 		 * Example : nums = [4,5,6,7,0,1,2], target = 0 
 		 * Output: 4
 		 * 
+		 * Brute force : linear search traverse whole array, return index when found the target.
+		 * 
+		 * Optimal : since array was originally sorted, so even when rotated, some part of array will still be sorted.
+		 * Idea : when array is rotated, then by each rotation start index of sorted array get shifted in the direction
+		 * of rotation.
+		 * 
+		 * so while we find the middle, every time. we know that either left or right part of middle will
+		 * always be sorted. we go search target in wrong direction only when target is not in range of 
+		 * sorted part of array.
 		 * 
 		 * */
 		
@@ -142,5 +151,50 @@ public class Search {
         }
 
         return -1;
+    }
+	
+	public boolean searchInRotatedSortedArrayWithDuplicateElements(int[] nums, int target) {
+		/*
+		 * extending to above problem, when array has duplicate elements, then 
+		 * for a particular case, it is possible that we can not tell which part might be sorted.
+		 * 
+		 * E.g: [3,4,5,6,3,3,3,3,3] 
+		 * here nums[0] = 3 : low
+		 * 		nums[4] = 3 : mid
+		 * 		nums[7] = 3 : right
+		 * 
+		 * Solution : move left, right by one, and narrow down the search space.
+		 * 
+		 * */
+        int left = 0;
+        int right = nums.length-1;
+
+        while(left<=right){
+            int mid = (left+right)/2;
+            if(nums[mid] == target)
+                return true;
+
+            if(nums[left] == nums[mid] && nums[mid] == nums[right]){
+                left++;
+                right--;
+                continue;
+            }
+            
+            if(nums[left] <= nums[mid]){
+                //array is sorted on left side.
+                if(nums[left] <= target && target < nums[mid])
+                    right = mid-1;
+                else
+                    left = mid+1;
+            }
+            else{
+                //array is sorted on right side.
+                if(target > nums[mid] && target <= nums[right])
+                    left = mid+1;
+                else
+                    right = mid-1;
+            }
+        }
+        return false;
     }
 }
