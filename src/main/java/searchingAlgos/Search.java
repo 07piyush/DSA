41eae23d4bff5b1a062640f609d3d11aff652dfc
Piyush Sharma <sharma.076piyush@gmail.com>
2024-11-13 09:53:54 +0530

    Capacity To Ship Packages Within D Days

--- src/main/java/searchingAlgos/Search.java
+++ src/main/java/searchingAlgos/Search.java
@@ -452,4 +452,78 @@ public class Search {
          }
          return minMax;
      }
+    
+    public static int shipWithinDays(int[] weights, int days) {
+    	/*
+    	 * A conveyor belt has packages that must be shipped from one port to another within days days. 
+    	 * The ith package on the conveyor belt has a weight of weights[i]. Each day, we load the ship with packages 
+    	 * on the conveyor belt (in the order given by weights). We may not load more weight than the maximum weight 
+    	 * capacity of the ship. Return the least weight capacity of the ship that will result in all the packages on 
+    	 * the conveyor belt being shipped within days days.
+    	 * 
+    	 * Input: weights = [1,2,3,4,5,6,7,8,9,10], days = 5 Output: 15
+    	 * Explanation: A ship capacity of 15 is the minimum to ship all the packages in 5 days like this:
+    	 * 1st day: 1, 2, 3, 4, 5
+    	 * 2nd day: 6, 7
+    	 * 3rd day: 8
+    	 * 4th day: 9
+    	 * 5th day: 10
+    	 * 
+    	 * brute force : for each weight on conveyer belt, let that weight be capacity of ship.
+    	 * thus for that selected capacity, check if we can deliver all items in given days.
+    	 * 
+    	 * if yes, use to compare with minimum capacity initialized before.
+    	 * and return.
+    	 * 
+    	 * Time = O(n2) | Space = O(1)
+    	 * 
+    	 * Optimal : maximum possible capacity could be sum of all item weights. while least capacity has to be 
+    	 * minimum weight on conveyer belt. now search for best weight in this range using binary search. and return
+    	 * 
+    	 * Time = O(n * log n) | Space = O(1)
+
+    	 * 
+    	 * */
+        int[] minMax = getMinMax(weights);
+        int low = minMax[1];
+        int high = sum(weights);
+        int minDays = high;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(checkIfEligibleShipCapacity(weights, days, mid)){
+                minDays = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return minDays;
+    }
+
+    public static int sum(int[] weights){
+        int sum = 0;
+        for(int i=0; i<weights.length; i++){
+            sum += weights[i];
+        }
+        return sum;
+    }
+
+    public static boolean checkIfEligibleShipCapacity(int[] weights, int days, int capacity){
+        int consumedPerDay = 0;
+        int requiredDays = 1;
+        for(int i=0; i<weights.length; i++){
+            if(consumedPerDay+weights[i] <= capacity){
+                consumedPerDay += weights[i];
+            }
+            else{
+                consumedPerDay = weights[i];
+                requiredDays++;
+            }
+        }
+        if(requiredDays <= days)
+            return true;
+        else
+            return false;
+    }
 }
