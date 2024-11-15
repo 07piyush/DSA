36b182a948412e565a1f45965c1c98466f1f87b9
Piyush Sharma <sharma.076piyush@gmail.com>
2024-11-15 07:22:45 +0530

    Find Kth largest element in array

--- src/main/java/heaps/kthLargestSmallest.java
+++ src/main/java/heaps/kthLargestSmallest.java
@@ -1,5 +1,7 @@
 package heaps;
 
+import java.util.PriorityQueue;
+
 public class kthLargestSmallest {
 	/*
 	 * Heap : by definition always has largest/smallest element at the top. So for MinHeap top will be smallest element.
@@ -16,8 +18,34 @@ public class kthLargestSmallest {
 	 * since heap size is fixed, so any greater element will be rejected, and at the end of array traversal heap will
 	 * have kth smallest at top.
 	 * 
+	 * since for any problem with input size N, when answer lies at top K size then untill K size is reached
+	 * we will add an element in heap if element is smaller than top of heap, and maintain protocol of heap,
+	 * we will sort the heap (heapify). 
 	 * 
+	 * So time complexity reduces from nlogn to nlogK.
 	 * 
 	 * 
 	 * */
+	
+	public int findKthLargest(int[] nums, int k) {
+		/*
+		 * Given an integer array nums and an integer k, return the kth largest element in the array.
+		 * Note that it is the kth largest element in the sorted order, not the kth distinct element.
+		 * 
+		 * Brute force : 1. create duplicate of nums. sort nums2, find unique kth item from right.
+		 * Time = N (duplicating) + NlogN (sorting) + N (finding) | Space = N
+		 * 2. create an array with unique elements of nums, sort it, return size-k-1 element.
+		 * Time = N(duplicating) + Nlogn.  |  Space = N
+		 * 
+		 * Optimal : using minHeap. Heap always stores unique values, it would never keep duplicate items.
+		 * 
+		 * */
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for(int num : nums){
+            minHeap.add(num);
+            if(minHeap.size() > k)
+                minHeap.poll();
+        }
+        return minHeap.peek();
+    }
 }
