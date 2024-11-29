1de05371604646ecf44aff0b13b45debeac10087
Piyush Sharma <sharma.076piyush@gmail.com>
2024-11-29 10:02:53 +0530

    GFG Minimum Platforms - greedy

--- src/main/java/greedy/Greedy.java
+++ src/main/java/greedy/Greedy.java
@@ -337,4 +337,45 @@ public class Greedy {
 		}
         return jumps;
     }
+	
+	static int findPlatform(int arr[], int dep[]) {
+		/*
+		 * You are given the arrival times arr[] and departure times dep[] of all trains that arrive at a railway 
+		 * station on the same day. Your task is to determine the minimum number of platforms required at the station 
+		 * to ensure that no train is kept waiting.
+		 * 
+		 * At any given time, the same platform cannot be used for both the arrival of one train and the departure of 
+		 * another. Therefore, when two trains arrive at the same time, or when one arrives before another departs, 
+		 * additional platforms are required to accommodate both trains.
+		 * 
+		 * Input: arr[] = [900, 940, 950, 1100, 1500, 1800], dep[] = [910, 1200, 1120, 1130, 1900, 2000]
+		 * Output: 3
+		 * Explanation: There are three trains during the time 9:40 to 12:00. So we need a minimum of 3 platforms.
+		 * 
+		 * Brute Force : for each incoming train find if there is anyoverlapping time, increase platforms if yes.
+		 * 
+		 * TODO : complete function as per coding standards.
+		 * 
+		 * */
+		
+        int ar = 0;
+        int dp = 0;
+        int max = 0;
+        int plat = 0;
+        Arrays.sort(arr);
+        Arrays.sort(dep);
+        while(ar<arr.length && dp<dep.length){
+            
+            if(arr[ar] <= dep[dp]){
+                ar++;
+                plat++;
+                max = max>plat?max:plat;
+            }
+            else{
+                dp++;
+                plat--;
+            }
+        }
+        return max;
+    }
 }
